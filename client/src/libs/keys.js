b936b71c6fea34d6f3f3ab12a0961035431d9877
Marvin Kome <marvinkome@gmail.com>
2020-05-10 22:49:10 +0100

    update key in client

--- client/src/libs/keys.js
+++ client/src/libs/keys.js
@@ -1,7 +1,7 @@
 const __DEV__ = process.env.NODE_ENV === 'development';
 
 export const AUTH_TOKEN_KEY = 'contentlify_auth_token';
-export const API_URL = __DEV__ ? '' : '';
+export const API_URL = __DEV__ ? 'http://localhost:7077' : 'https://mycontento.com/api';
 export const APP_URL = __DEV__ ? 'http://localhost:3033' : 'https://mycontento.com';
 export const GITHUB_CLIENT_KEY = __DEV__ ? 'edb690e9e92ffbbe926d' : 'c7a5dd1f5fd110551912';
 export const GOOGLE_CLIENT_KEY = __DEV__
