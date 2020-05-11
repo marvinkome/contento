b69b146c6d3ddec4a80efaf79ffd581d7ed8977b
Marvin Kome <marvinkome@gmail.com>
2020-05-11 01:01:19 +0100

    update prod setup

--- prod-setup.sh
+++ prod-setup.sh
@@ -1,10 +1,8 @@
 #! /bin/bash
 
-alias docker-compose-prod="docker-compose -f docker-compose.yml -f docker-compose.prod.yml"
-
 # stop previous containers
 docker-compose stop
 docker-compose rm -f
  
 # restart container
-docker-compose-prod up -d
+docker-compose -f docker-compose.yml -f docker-compose.prod.yml up -d
