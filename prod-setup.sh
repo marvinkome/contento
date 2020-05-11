3fc8d9ab52a541ed73e956c6c9239e51921bde2f
Marvin Kome <marvinkome@gmail.com>
2020-05-11 01:04:23 +0100

    update prod script

--- prod-setup.sh
+++ prod-setup.sh
@@ -3,6 +3,7 @@
 # stop previous containers
 docker-compose stop
 docker-compose rm -f
+docker-compose down --rmi all
  
 # restart container
 docker-compose -f docker-compose.yml -f docker-compose.prod.yml up -d
