7dc47c0f9a88de3097dacd75c1c897841309f439
Marvin Kome <marvinkome@gmail.com>
2020-07-09 02:51:00 +0100

    trigger build

--- server/src/index.ts
+++ server/src/index.ts
@@ -38,7 +38,7 @@ export default function createApp() {
     setup_cloudinary();
 
     // api routes
-    app.use('/', (_, res) => res.json({ message: "You're at contento server" }));
+    app.use('/', (_, res) => res.json({ message: "You're at contento server v1.0" }));
     app.use('/api', routes);
 
     return { app, apolloServer };
