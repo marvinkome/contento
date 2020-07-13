233d4335d9f51cb838503db3c5101f1841a5b05d
Marvin Kome <marvinkome@gmail.com>
2020-07-13 23:21:54 +0100

    fix email templates

--- server/copyStaticAssets.ts
+++ server/copyStaticAssets.ts
@@ -1,4 +1,4 @@
 import * as shell from 'shelljs';
 
 shell.mkdir('dist/src/libs/emails/templates');
-shell.cp('-R', 'src/libs/emails/templates', 'dist/src/libs/emails/templates/');
+shell.cp('-R', 'src/libs/emails/templates', 'dist/src/libs/emails/');
