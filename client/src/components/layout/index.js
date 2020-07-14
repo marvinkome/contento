fbf6bbaf6b2da84fa6c277698d47d1be2af81564
Marvin Kome <marvinkome@gmail.com>
2020-07-14 23:03:00 +0100

    make dashboard non-responsive

--- client/src/components/layout/index.js
+++ client/src/components/layout/index.js
@@ -1,17 +1,25 @@
-import React from 'react';
+import React, { useLayoutEffect } from 'react';
 import Topbar from './topbar';
 import Sidebar from './sidebar';
 
 import './layout.scss';
 
-export default class Layout extends React.Component {
-    render() {
-        return (
-            <div className="dashboard__container">
-                <Topbar />
-                <Sidebar />
-                <div className="dashboard__main">{this.props.children}</div>
-            </div>
-        );
-    }
+export default function Layout(props) {
+    useLayoutEffect(() => {
+        document
+            .querySelector('meta[name="viewport"]')
+            .setAttribute(
+                'content',
+                'width=1024, initial-scale=0, maximum-scale=1.0, minimum-scale=0.25, ' +
+                    'user-scalable=yes'
+            );
+    });
+
+    return (
+        <div className="dashboard__container">
+            <Topbar />
+            <Sidebar />
+            <div className="dashboard__main">{props.children}</div>
+        </div>
+    );
 }
